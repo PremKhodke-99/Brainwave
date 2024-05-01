1cef858741408772ccf7e4192693d040128a5602
Prem <premkhodke.99@gmail.com>
2024-05-01 10:06:01 +0530

    Services

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import ButtonGradient from './assets/svg/ButtonGradient'
 import Benefits from './components/Benefits'
-import Button from './components/Button'
 import Collaboration from './components/Collaboration'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Services from './components/Services'
 
 const App = () => {
-
   return (
     <>
       <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
@@ -14,6 +13,7 @@ const App = () => {
         <Hero />
         <Benefits />
         <Collaboration />
+        <Services />
       </div>
       <ButtonGradient />
     </>
