394aac0c0a1d869454d54ab79d1ceb39361e8672
Prem <premkhodke.99@gmail.com>
2024-05-01 13:11:19 +0530

    roadmap, footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import ButtonGradient from './assets/svg/ButtonGradient'
 import Benefits from './components/Benefits'
 import Collaboration from './components/Collaboration'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Pricing from './components/Pricing'
+import Roadmap from './components/Roadmap'
 import Services from './components/Services'
 
 const App = () => {
@@ -14,6 +17,9 @@ const App = () => {
         <Benefits />
         <Collaboration />
         <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
